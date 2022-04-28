79fb992e0ade0c6e917505e88fecd8966b68474d
thomas nadin-hepburn <tom.nadin.hepburn@gmail.com>
2022-04-28 14:39:18 +0100

    created coordinate class

--- hand_detection.py
+++ hand_detection.py
@@ -1,12 +1,37 @@
 import cv2
 import mediapipe as mp
 
+class LandmarkCoordinates(object):
+    def __init__(self,x,y,z):
+        self.X = x
+        self.Y = y
+        self.Z = z
+
+    def __add__(self,other):
+        return [self.X + other.X, self.Y + other.Y, self.Z + other.Z]
+
+    def __str__(self):
+        return(str(self.X)+", "+str(self.Y)+", "+str(self.Z))
+        
+    def getXYZ(self):
+        return [self.X,self.Y,self.Z]
+
+    def getX(self):
+        return self.X
+
+    def getY(self):
+        return self.Y
+
+    def getZ(self):
+        return self.Z
+
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 # For static images:
-IMAGE_FILES = ["archive\\asl_alphabet_train\\asl_alphabet_train\\B\\B3.jpg"]
+IMAGE_FILES = ["B\\B3.jpg"]
 with mp_hands.Hands(
     static_image_mode=True,
     max_num_hands=1,
@@ -14,6 +39,7 @@ with mp_hands.Hands(
 
     for idx, file in enumerate(IMAGE_FILES):
         one_image = []
+        class_one_image = []
         # Read an image, flip it around y-axis for correct handedness output (see
         # above).
         image = cv2.flip(cv2.imread(file), 1)
@@ -26,13 +52,36 @@ with mp_hands.Hands(
             for landmark in hand_landmarks.landmark:
                 one_image.append([landmark.x,landmark.y,landmark.z])
         print(one_image)
-        fingercoords = []
-        for i in range (5):
-            changesx = one_image[0][0]+one_image[i*4+1][0]+one_image[i*4+2][0]+one_image[i*4+3][0]+one_image[i*4+4][0]
-            changesy = one_image[0][1]+one_image[i*4+1][1]+one_image[i*4+2][1]+one_image[i*4+3][1]+one_image[i*4+4][1]
-            changesz = one_image[0][2]+one_image[i*4+1][2]+one_image[i*4+2][2]+one_image[i*4+3][2]+one_image[i*4+4][2]
-            fingercoords.append([changesx,changesy,changesz])
-        print(fingercoords)
+        wristToThumbBase = [one_image[0],one_image[1]]
+        wristToIndexBase = [one_image[0],one_image[5]]
+        wristToLittleBase = [one_image[0],one_image[17]]
+        thumbBaseToThumbTip = [one_image[1],one_image[4]]
+        indexBaseToIndexTip = [one_image[5],one_image[8]]
+        middleBaseToMiddleTip = [one_image[9],one_image[12]]
+        ringBaseToRingTip = [one_image[13],one_image[16]]
+        littleBaseToLittleTip = [one_image[17],one_image[20]]
+
+        
+        for hand_landmarks in results.multi_hand_landmarks:
+            for landmark in hand_landmarks.landmark:
+                class_one_image.append(LandmarkCoordinates(landmark.x,landmark.y,landmark.z))
+        
+        wristToThumbBase = [class_one_image[0]+class_one_image[1]]
+        wristToIndexBase = [class_one_image[0]+class_one_image[5]]
+        wristToLittleBase = [class_one_image[0]+class_one_image[17]]
+        thumbBaseToThumbTip = [class_one_image[1]+class_one_image[4]]
+        indexBaseToIndexTip = [class_one_image[5]+class_one_image[8]]
+        middleBaseToMiddleTip = [class_one_image[9]+class_one_image[12]]
+        ringBaseToRingTip = [class_one_image[13]+class_one_image[16]]
+        littleBaseToLittleTip = [class_one_image[17]+class_one_image[20]]
+
+        # fingercoords = []
+        # for i in range (5):
+        #     changesx = one_image[0][0]+one_image[i*4+1][0]+one_image[i*4+2][0]+one_image[i*4+3][0]+one_image[i*4+4][0]
+        #     changesy = one_image[0][1]+one_image[i*4+1][1]+one_image[i*4+2][1]+one_image[i*4+3][1]+one_image[i*4+4][1]
+        #     changesz = one_image[0][2]+one_image[i*4+1][2]+one_image[i*4+2][2]+one_image[i*4+3][2]+one_image[i*4+4][2]
+        #     fingercoords.append([changesx,changesy,changesz])
+        # print(fingercoords)
 
 
 
