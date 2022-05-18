c24dbbc0411b05397e15deb1197a4347affb500b
tnh <tomnadinhepburn@gmail.com>
2022-05-18 14:55:46 +0100

    updated files

--- landmark_coordinates.py
+++ landmark_coordinates.py
@@ -1,38 +1,6 @@
 import cv2, csv
 import mediapipe as mp
 
-class landmarkCoordinates(object):
-    def __init__(self,x,y,z):
-        self.X = x
-        self.Y = y
-        self.Z = z
-
-    def __add__(self,other):
-        return [self.X + other.getX(), self.Y + other.getY(), self.Z + other.getZ()]
-
-    def __mul__(self,other):
-        return self.X * other.X+ self.Y * other.Y+ self.Z * other.Z
-
-    def __str__(self):
-        return(str(self.X)+", "+str(self.Y)+", "+str(self.Z))
-        
-    def getXYZ(self):
-        return [self.X,self.Y,self.Z]
-
-    def getX(self):
-        return self.X
-
-    def getY(self):
-        return self.Y
-
-    def getZ(self):
-        return self.Z
-
-class landmark_vector(object):
-    def __init__(self,magnitude,direction):
-        self.Magnitude = magnitude
-        self.Direction = direction
-
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
@@ -77,84 +45,6 @@ with open(f'landmark_test_data.csv', 'w') as data_file:
     for row in EXTRACTED_DATA:
         #print(row)
         writeFile.writerow(row)
-
-
-
-
-
-
-
-
-
-
-
-        # wristToThumbBase = [one_image[0],one_image[1]]
-        # wristToIndexBase = [one_image[0],one_image[5]]
-        # wristToLittleBase = [one_image[0],one_image[17]]
-        # thumbBaseToThumbTip = [one_image[1],one_image[4]]
-        # indexBaseToIndexTip = [one_image[5],one_image[8]]
-        # middleBaseToMiddleTip = [one_image[9],one_image[12]]
-        # ringBaseToRingTip = [one_image[13],one_image[16]]
-        # littleBaseToLittleTip = [one_image[17],one_image[20]]
-
-
-
-
-
-
-
-
-
-        
-        # for hand_landmarks in results.multi_hand_landmarks:
-        #     for landmark in hand_landmarks.landmark:
-        #         class_one_image.append(landmark_coordinates(landmark.x,landmark.y,landmark.z))
-
-
-        # ###
-        # wristToThumbBase = [class_one_image[1]+class_one_image[0]]
-        # wristToIndexBase = [class_one_image[5]+class_one_image[0]]
-        # wristToLittleBase = [class_one_image[17]+class_one_image[0]]
-        # ###
-
-        # ###
-        # thumbBaseToThumbPIP = [class_one_image[2]+class_one_image[1]]
-        # thumbPIPToThumbDIP = [class_one_image[3]+class_one_image[2]]
-        # thumbDIPToThumbTip = [class_one_image[4]+class_one_image[3]]
-        # ###
-
-        # ###
-        # indexBaseToIndexPIP = [class_one_image[6]+class_one_image[5]]
-        # indexPIPToIndexDIP = [class_one_image[7]+class_one_image[6]]
-        # indexDIPToIndexTip = [class_one_image[8]+class_one_image[7]]
-        # ###
-
-        # ###
-        # middleBaseToMiddlePIP = [class_one_image[10]+class_one_image[9]]
-        # middlePIPToMiddleDIP = [class_one_image[11]+class_one_image[10]]
-        # middleDIPToMiddleTip = [class_one_image[12]+class_one_image[11]]
-        # ###
-
-        # ###
-        # ringBaseToRingPIP = [class_one_image[14]+class_one_image[13]]
-        # ringPIPToRingDIP = [class_one_image[15]+class_one_image[14]]
-        # ringDIPToRingTip = [class_one_image[16]+class_one_image[15]]
-        # ###
-
-        # ###
-        # littleBaseToLittlePIP = [class_one_image[18]+class_one_image[17]]
-        # littlePIPToLittleDIP = [class_one_image[19]+class_one_image[18]]
-        # littleDIPToLittleTip = [class_one_image[20]+class_one_image[19]]
-        # ###
-
-        # fingercoords = []
-        # for i in range (5):
-        #     changesx = one_image[0][0]+one_image[i*4+1][0]+one_image[i*4+2][0]+one_image[i*4+3][0]+one_image[i*4+4][0]
-        #     changesy = one_image[0][1]+one_image[i*4+1][1]+one_image[i*4+2][1]+one_image[i*4+3][1]+one_image[i*4+4][1]
-        #     changesz = one_image[0][2]+one_image[i*4+1][2]+one_image[i*4+2][2]+one_image[i*4+3][2]+one_image[i*4+4][2]
-        #     fingercoords.append([changesx,changesy,changesz])
-        # print(fingercoords)
-
 
 
 
@@ -221,8 +111,7 @@ with open(f'landmark_test_data.csv', 'w') as data_file:
 #                 mp_drawing_styles.get_default_hand_landmarks_style(),
 #                 mp_drawing_styles.get_default_hand_connections_style())
 
-
-    
+ 
 #     cv2.imwrite(
 #         '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
 #     # Draw hand world landmarks.
