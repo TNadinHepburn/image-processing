b038a542f87a8ca9f5ab3d425414adb57daaf5d5
tnh <tomnadinhepburn@gmail.com>
2022-05-23 10:56:44 +0100

    added test img to dir & cleaned predict/model

--- network-model.py
+++ network-model.py
@@ -16,18 +16,22 @@ import os, shutil, random, glob, matplotlib.pyplot as plt, numpy as np
 labels = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 validation_percent = 0.1
 batch_size = 50
-target_size = (64, 64)
-target_dims = (64, 64, 3) # add channel for RGB
+target_size = (128, 128)
+target_dims = (128, 128, 3) # add channel for RGB
 n_classes = 26
-epoch = 5
-data_dir = 'asl_alphabet_train'
+epoch = 10
+data_dir = 'D:/asl_alphabet_train'
 test_data_dir = 'asl_alphabet_test'
 data_augmentor = ImageDataGenerator(samplewise_center=True, 
                                     samplewise_std_normalization=True, 
                                     validation_split=validation_percent)
 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print(len(physical_devices))
+tf.config.experimental.set_memory_growth(physical_devices[0],True)
+
 def plotImages(images):
-    fig, axes = plt.subplots(8,8,figsize=(64,64))
+    fig, axes = plt.subplots(8,8,figsize=(128,128))
     axes = axes.flatten()
     for img, ax in zip(images, axes):
         ax.imshow(img)
@@ -55,21 +59,8 @@ def expandTestSet(numToExpand):
         for c in random.sample(glob.glob('asl_alphabet_train/'+label+'/*'),numToExpand):
             shutil.move(c, 'asl_alphabet_test/'+label)
 
-def saveModel():
-    my_model.save('asl_model.h5')
-
-
-train_generator = data_augmentor.flow_from_directory(data_dir, target_size=target_size, batch_size=batch_size, subset="train") 
-val_generator = data_augmentor.flow_from_directory(data_dir, target_size=target_size, batch_size=batch_size, subset="val")
-test_generator = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=test_data_dir, target_size=target_size, batch_size=batch_size, shuffle=False)
-
-img, label = next(train_generator)
-
-# plotImages(img)
-# print("-------------LABELS-------------")
-# print(label)
-# print("-------------LABELS-------------")
-
+train_generator = data_augmentor.flow_from_directory(data_dir, target_size=target_size, batch_size=batch_size, subset="training") 
+val_generator = data_augmentor.flow_from_directory(data_dir, target_size=target_size, batch_size=batch_size, subset="validation")
 
 my_model = Sequential()
 my_model.add(Conv2D(64, kernel_size=4, strides=1, activation='relu', input_shape=target_dims))
@@ -89,19 +80,4 @@ my_model.add(Dense(n_classes, activation='softmax'))
 my_model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=["accuracy"])
 my_model.fit(x=train_generator, validation_data=val_generator, shuffle=True, epochs=epoch, verbose=1)
 
-
-test_img, test_label = next(test_generator)
-
-predictions = my_model.predict(test_generator, verbose=0)
-np.round(predictions)
-print("PREDICTIONS")
-print(predictions)
-print("ARGMAX PREDICTIONS")
-print(np.argmax(predictions, axis=-1))
-print(labels[np.argmax(predictions, axis=-1)])
-
-cm = confusion_matrix(y_true=test_generator.classes, y_pred=np.argmax(predictions, axis=-1))
-cm_plot_labels = labels
-print("matrix")
-plot_confusion_matrix(cm=cm, classes=cm_plot_labels,title="Confusion")
-print("done matrix\nsaved")
+my_model.save('asl_model.h5')
--- predict.py
+++ predict.py
@@ -1,12 +1,32 @@
-import numpy as np
+import matplotlib.pyplot as plt, numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from keras.preprocessing.image import ImageDataGenerator
+from math import sqrt, ceil
 
-my_model = keras.models.load_model("my_model")
-img_for_pred = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=test_data_dir, target_size=target_size, batch_size=batch_size, shuffle=False)
+labels = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+target_size = (64,64)
+test_data_dir = 'asl_alphabet_test'
+my_model = keras.models.load_model("asl_model.h5")
+img_for_pred = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=test_data_dir, target_size=target_size, batch_size=1, shuffle=False)
 
-
+def plotImages(images):
+    subplot_size_x = ceil(sqrt(len(images)))
+    if subplot_size_x > 5:
+        subplot_size_y = ceil(len(images)/5)
+        subplot_size_x = 5
+    else:
+        subplot_size_y = subplot_size_x
+    if subplot_size_x == 1 and subplot_size_y == 1:
+        plt.imshow(images[0])
+    else:
+        fig, axes = plt.subplots(5,5,figsize=(64,64))
+        axes = axes.flatten()
+        for img, ax in zip(images, axes):
+            ax.imshow(img)
+            ax.axis('off')
+    plt.tight_layout()
+    plt.show()
 
 
 predictions = my_model.predict(img_for_pred, verbose=0)
@@ -15,4 +35,9 @@ print("PREDICTIONS")
 print(predictions)
 print("ARGMAX PREDICTIONS")
 print(np.argmax(predictions, axis=-1))
-print(labels[np.argmax(predictions, axis=-1)])
+count = 0
+for result in np.argmax(predictions, axis=-1):
+    print(labels[result])
+    print(img_for_pred[count][1])
+    plotImages(img_for_pred[count][0])
+    count +=1
