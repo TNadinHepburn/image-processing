4d345549109176154f2b625540dbde352ae41357
LukasTrišauskas <76224796+Lukas-Trisauskas@users.noreply.github.com>
2022-05-25 13:00:48 +0100

    v0.2.0
    adjustments

--- interface.py
+++ interface.py
@@ -44,7 +44,7 @@ window.config(padx=10, pady=10)
 
 # canvas setup
 # width and height is based on the frame size
-webcam = tk.Canvas(window, width=400, height=290)
+webcam = tk.Canvas(window, width=400, height=290, background='gray')
 # positioning canvas on the grid
 webcam.grid(column=0, row=0, padx=5, pady=5)
 
@@ -96,25 +96,27 @@ browse_btn.grid(column=1, row=1)
 screenshot_btn.grid(column=0, row=3)
 exit_btn.grid(column=0, row=6)
 
-def update():
-    # reads stream and returns true if read, and the frame data
-    ret, frame = cap.read()
-    # frame returned
-    if ret:
-        # covert the frame from bgr to rgba
-        convert_to_rgba = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-        # convert from array to image and then to PhotoImage object for tkinter
-        photo = ImageTk.PhotoImage(image=Image.fromarray(convert_to_rgba))
-        # anchor the image (otherwise gets deleted by garbage collection)
-        webcam.photo = photo
-        # display the image on the canvas
-        webcam.create_image(0, 0, image=photo, anchor='nw')
-    # updates the canvas with a new frame every 30 miliseconds
-    window.after(30, update)
+# def update():
+#     # reads stream and returns true if read, and the frame data
+#     ret, frame = cap.read()
+#     # frame returned
+#     if ret:
+#         # covert the frame from bgr to rgba
+#         convert_to_rgba = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+#         # convert from array to image and then to PhotoImage object for tkinter
+#         photo = ImageTk.PhotoImage(image=Image.fromarray(convert_to_rgba))
+#         # anchor the image (otherwise gets deleted by garbage collection)
+#         webcam.photo = photo
+#         # display the image on the canvas
+#         webcam.create_image(0, 0, image=photo, anchor='nw')
+#     # updates the canvas with a new frame every 30 miliseconds
+#     window.after(30, update)
 
 # once the porotocol for closing window is initiated a call back function is called
 window.protocol('WM_DELETE_WINDOW', destructor)
 
-update()
+#update()
+window.resizable(False, False)
+window.mainloop()
 window.resizable(False, False)
 window.mainloop()
